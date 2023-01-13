0a334e800efefde1f1884af34bb36ad452176f36
AltynZhanbyr <altyn.bekzat01@gmail.com>
2023-01-13 16:03:41 +0600

    Firebase methods added

--- app/src/main/java/com/example/newchatapp/view/LoginFragment.kt
+++ app/src/main/java/com/example/newchatapp/view/LoginFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.newchatapp.AppKeys
 import com.example.newchatapp.R
 import com.example.newchatapp.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
@@ -24,12 +27,18 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater)
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        userDatabase = firebaseDatabase.getReference(AppKeys.USER_KEY)
         return binding?.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onStart() {
+        super.onStart()
 
+        auth = Firebase.auth
+        if(auth!=null){
+
+        }
     }
 
     override fun onDestroy() {
