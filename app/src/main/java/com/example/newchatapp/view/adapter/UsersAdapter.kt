e54aa5ba1a8244558a183feba29f49f97fa72e30
AltynZhanbyr <altyn.bekzat01@gmail.com>
2023-01-15 20:48:47 +0600

    added users adapter

---
+++ app/src/main/java/com/example/newchatapp/view/adapter/UsersAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.newchatapp.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newchatapp.R
+
+class UsersAdapter():RecyclerView.Adapter<UsersAdapter.UserViewHolder>() {
+    inner class UserViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
