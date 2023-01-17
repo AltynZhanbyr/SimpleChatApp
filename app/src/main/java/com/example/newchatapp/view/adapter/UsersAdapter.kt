160d60130c54794cb1a5052efe898343311ae61c
AltynZhanbyr <altyn.bekzat01@gmail.com>
2023-01-17 14:04:00 +0600

    added recyclerview adapter

--- app/src/main/java/com/example/newchatapp/view/adapter/UsersAdapter.kt
+++ app/src/main/java/com/example/newchatapp/view/adapter/UsersAdapter.kt
@@ -5,21 +5,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newchatapp.R
+import com.example.newchatapp.databinding.UserItemBinding
+import com.example.newchatapp.model.User
 
-class UsersAdapter():RecyclerView.Adapter<UsersAdapter.UserViewHolder>() {
+class UsersAdapter(private val userList:MutableList<User>):RecyclerView.Adapter<UsersAdapter.UserViewHolder>() {
     inner class UserViewHolder(view: View): RecyclerView.ViewHolder(view){
+        private val binding:UserItemBinding = UserItemBinding.bind(view)
 
+        fun bind(user:User){
+            binding.apply {
+                this.userName.text = user.userFirstName+" "+user.userLastName
+                this.userStatus.text = "online"
+
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
+        return UserViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val user = userList[position]
+        holder.bind(user)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return this.userList.size
     }
 }
