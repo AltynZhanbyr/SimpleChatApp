ab362df1a6179438d6a11473096b8dc498eef27f
AltynZhanbyr <altyn.bekzat01@gmail.com>
2023-01-24 21:32:18 +0600

    list of users updated

--- app/src/main/java/com/example/newchatapp/viewmodel/UsersViewModel.kt
+++ app/src/main/java/com/example/newchatapp/viewmodel/UsersViewModel.kt
@@ -2,6 +2,7 @@ package com.example.newchatapp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.newchatapp.AppKeys
 import com.example.newchatapp.model.User
 import com.google.firebase.auth.FirebaseAuth
@@ -9,6 +10,9 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class UsersViewModel:ViewModel() {
     private val firebaseAuth: FirebaseAuth
@@ -37,13 +41,12 @@ class UsersViewModel:ViewModel() {
                 if(userList.size>0)
                     userList.clear()
                 for(data in p0.children){
-                    userList.add(data.getValue(User::class.java)!!)
+                    if(data.getValue(User::class.java)!!.userId!=firebaseAuth.currentUser?.uid)
+                        userList.add(data.getValue(User::class.java)!!)
                 }
                 users.postValue(userList)
             }
-
             override fun onCancelled(p0: DatabaseError) {
-                throw Exception("Data loading error")
             }
         })
     }
@@ -76,15 +79,14 @@ class UsersViewModel:ViewModel() {
         }
     }
 
+    fun signOut(){
+        Firebase.auth.signOut()
+    }
+
     private fun createUser(user:FirebaseUser, name:String, lastName:String){
         val newUser = User(user?.uid,name,lastName,user?.email)
-        databaseReference.push().setValue(newUser)
-    }
-
-    fun updateUser(){
-
-    }
-    fun deleteUser(){
-
+        viewModelScope.launch(Dispatchers.IO) {
+            databaseReference.child(newUser.userId.toString()).setValue(newUser).await()
+        }
     }
 }
